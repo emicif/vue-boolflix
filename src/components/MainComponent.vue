6cb6ab9b965b2e49207677d920b48889031e215b
emicif <emanuela.cifolelli@gmail.com>
2022-04-20 13:56:16 +0200

    searchbar in main

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,15 +1,47 @@
 <template>
   <div>
-    Main
+    <input type=text v-model="query" />
+    <button @click="queryApi">Cerca</button>
+    <div v-for="item in query" :key="item.id"> 
+      <h2>{{item.name}}</h2>
+      <p>{{item.original_name}}</p>
+      <span>{{item.original_language}}</span>
+
+    </div>
   </div>
   
 </template>
 
 <script>
+
+import axios from 'axios';
+
+
 export default {
   name: 'MainComponent',
-  props: {
-    msg: String
+   data(){
+    return {
+      apiUrl: 'https://api.themoviedb.org/3/search/',
+      apiKey: '5125681fd07a81d9933e9268c5c370c3',
+      query: '',
+    }
+  },
+  methods: {
+    queryApi(){
+const params = {
+      query: this.query,
+      api_key: this.apiKey,
+      language: 'it-IT'
+    }
+    axios.get(this.apiUrl + 'tv', {params}).then((response)=>{
+      console.log(response);
+      this.query = response.data.results
+    }).catch(error=>{
+      console.log(error);
+    })
+    }
+   // const query = 'verde';
+    
   }
 }
 </script>
